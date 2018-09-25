7211a45d13b1c217598365a262452a27e0a69b41
ryalia44 <Ryalia888@gmail.com>
2018-09-25 13:03:09 -0700

    DrugProNET v1.0.0.0 - RL - Major: Removed initial files

--- DrugProNET/Global.asax.cs
+++ DrugProNET/Global.asax.cs
@@ -13,9 +13,7 @@ namespace DrugProNET
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
