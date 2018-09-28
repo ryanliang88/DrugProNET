965bb83f3cc0f4ae01e9349ea40f7be598ef1e35
Andy Tang <andytang43@gmail.com>
2018-09-28 15:12:58 -0700

    added masterpage and set it up with the content placeholder

--- DrugProNET/DrugInfo.aspx.designer.cs
+++ DrugProNET/DrugInfo.aspx.designer.cs
@@ -11,5 +11,77 @@ namespace DrugProNET {
     
     
     public partial class DrugInfo {
+        
+        /// <summary>
+        /// script_manager control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager script_manager;
+        
+        /// <summary>
+        /// ad_refresh_timer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.Timer ad_refresh_timer;
+        
+        /// <summary>
+        /// ad_update_panel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel ad_update_panel;
+        
+        /// <summary>
+        /// ad_link control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink ad_link;
+        
+        /// <summary>
+        /// ad_banner control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image ad_banner;
+        
+        /// <summary>
+        /// search_textBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox search_textBox;
+        
+        /// <summary>
+        /// retrieve_button control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button retrieve_button;
+        
+        /// <summary>
+        /// reset_button control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button reset_button;
     }
 }
