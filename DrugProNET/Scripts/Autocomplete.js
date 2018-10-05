788b6d920406cdd2e22f2eba62f3da9597d12025
Andy Tang <andytang43@gmail.com>
2018-10-04 18:55:12 -0700

    added connection to .aspx for autocomplete results

--- DrugProNET/Scripts/Autocomplete.js
+++ DrugProNET/Scripts/Autocomplete.js
@@ -1,17 +1,36 @@
 ﻿$(document).ready(function () {
     Sys.WebForms.PageRequestManager.getInstance().add_endRequest(autoComplete);
     function autoComplete(sender, args) {
-        var data = [
-            "Dog",
-            "Cat",
-            "Bird",
-            "Giraffe",
-            "Bowl",
-            "Big"
-        ];
+        //var data = [
+        //    "Dog",
+        //    "Cat",
+        //    "Bird",
+        //    "Giraffe",
+        //    "Bowl",
+        //    "Big"
+        //];
+
+        //$("#search_textBox").autocomplete({
+        //    source: data
+        //});
+
 
         $("#search_textBox").autocomplete({
-            source: data
+            source: function (request, response) {
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    url: "ProteinInfo.aspx/GetAutoCompleteData",
+                    data: "{'value': '" + document.getElementById('search_textBox').value + "'}",
+                    dataType: "json",
+                    success: function (data) {
+                        response(data.d);
+                    },
+                    error: function (result) {
+                        alert("error");
+                    }
+                });
+            }
         });
     }
 });
