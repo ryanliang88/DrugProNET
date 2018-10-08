bc6dde18505a002db43cf46b4b45ff481d4d19ab
Andy Tang <andytang43@gmail.com>
2018-10-08 00:27:12 -0700

    fixed some styling, included drug_info_result.css into project, compacted MatchFinder.cs List length comparison code

--- DrugProNET/Scripts/MatchFinder.cs
+++ DrugProNET/Scripts/MatchFinder.cs
@@ -8,7 +8,8 @@ namespace DrugProNET.Scripts
 {
     public class MatchFinder
     {
-        public static List<string> FindMatches(string prefix, List<string> dataList, int start, int count, Comparison<string> compare) {
+        public static List<string> FindMatches(string prefix, List<string> dataList, int start, int count, Comparison<string> compare)
+        {
             List<string> matches = new List<string>();
             foreach (string data in dataList)
             {
@@ -17,12 +18,7 @@ namespace DrugProNET.Scripts
 
             matches.Sort(Comparer<string>.Create(compare));
 
-            if (matches.Count < count)
-            {
-                return matches.GetRange(start, matches.Count);
-            }
-
-            return matches.GetRange(start, count);
+            return (matches.Count < count) ? matches.GetRange(start, matches.Count) : matches.GetRange(start, count);
         }
     }
 }
