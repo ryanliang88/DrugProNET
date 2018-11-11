14e0a5b1e20f687fa2e9b2454464eeae4ada7e6d
Andy Tang <andytang43@gmail.com>
2018-11-10 16:46:43 -0800

    Added autocomplete template to SNVDrugQuery pag

--- DrugProNET/SNVDrugQuery.aspx.designer.cs
+++ DrugProNET/SNVDrugQuery.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace DrugProNET {
         protected global::System.Web.UI.WebControls.TextBox snv_specification_textbox;
         
         /// <summary>
+        /// AutoCompleteExtender control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.AutoCompleteExtender AutoCompleteExtender;
+        
+        /// <summary>
         /// generate_table_button control.
         /// </summary>
         /// <remarks>
